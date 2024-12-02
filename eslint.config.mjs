508e1c2d75036ed7b872bdbed497d067f2646430
Hakim Saoudi <saoudihakim@gmail.com>
2024-12-02 01:00:06 +0100

    chore: Update eslint configuration to ignore build, dist, coverage, docs, and node_modules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,21 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
+  {
+    ignores: ["build", "dist", "coverage", "docs", "node_modules"],
+  },
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
   eslintPluginPrettierRecommended,
   eslintConfigPrettier, // eslint-config-prettier last
 ];
